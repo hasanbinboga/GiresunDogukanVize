9b6fbe3eaec7e191dfb63e059fa722135c86d52b
onderrdogukan <131820840+onderrdogukan@users.noreply.github.com>
2023-04-26 01:07:53 +0300

    Create DersIstatistik.java

---
+++ DersIstatistik.java
@@ -0,0 +1,45 @@
+public class DersIstatistik {
+    int notAA;
+    int notBA;
+
+    void setNotAA(int notAA) {
+        this.notAA = notAA;
+    }
+
+    void setNotBA(int notBA) {
+        this.notBA = notBA;
+    }
+
+    int getNotAA() {
+        return this.notAA;
+    }
+
+    int getNotBA() {
+        return this.notBA;
+    }
+
+    void baslikYazdir() {
+
+    }
+
+    void menuYazdir() {
+
+    }
+
+    void notSayisiGir(DersIstatistik ders) {
+
+    }
+
+    void metinselIstatistikYazdir(DersIstatistik ders) {
+
+    }
+
+    void grafikselIstatistikYazdir(DersIstatistik ders) {
+
+    }
+
+    void veriSifirla(DersIstatistik ders) {
+
+    }
+
+}
