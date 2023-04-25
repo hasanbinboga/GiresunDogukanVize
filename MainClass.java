b477de4f73f654da3c8df0279fbc906cf7fcccfe
onderrdogukan <131820840+onderrdogukan@users.noreply.github.com>
2023-04-26 00:47:55 +0300

    Güncellendi

--- MainClass.java
+++ MainClass.java
@@ -5,6 +5,6 @@ public class MainClass {
 
   public static void main(String[] args) {
     System.out.println(
-        "initial Kodu güncelledin. şimdi bunu gite göndereceksin. Bunun için dosyayı kaydediyoruz. Sonra sol tarafta üçüncü sıradaki menü seçeneği ile değişiklikleri görüyoruz veya githug desktop üzerinden gönderebiliyoruz.");
+        "initial Kodu güncelledin. şimdi bunu gite göndereceksin. Bunun için dosyayı kaydediyoruz. Sonra sol tarafta üçüncü sıradaki menü seçeneği ile değişiklikleri görüyoruz veya githug desktop üzerinden gönderebiliyoruz. ");
   }
 }
